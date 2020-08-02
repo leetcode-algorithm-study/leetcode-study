5088c653db8a02b2fdb2cb9c01b2f5e1799b92d9
Seokjin <fietch096@gmail.com>
2020-08-02 18:16:32 +0900

    Revert "week#2 optimization"
    This reverts commit 0fb0e6b8e9b66dd2fd78a74202f95705d870fdeb.

--- 20200801/longest-palindromic-substring-seok.cpp
+++ 20200801/longest-palindromic-substring-seok.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
-
-/*
-How to approach?
-
-Brute-Force
-
-*/
 
 class Solution {
 public:
@@ -26,8 +20,6 @@ public:
         }
         for (i = 0; i < s.size(); i++)
         {
-
-            // Step 1. First loop. If there is the same character on both sides, count two more. else if it is the first iteration, just count one.
             curLen = 0;
             prev = i;
             next = i + 1;
@@ -58,7 +50,7 @@ public:
                     start = prev + 1;
                 }
             }
-            // Step 2. Second loop. it is the same condition as above except for the sequence order because of input like "ccc".
+
             curLen = 0;
             prev = i;
             next = i + 1;
@@ -99,10 +91,6 @@ int main()
 {
     string input = "ccc";
     Solution s;
-    cout << s.longestPalindrome(input);
-    return 0;
+	cout << s.longestPalindrome(input);
+	return 0;
 }
-/*
-Runtime: 48 ms, faster than 83.80 % of C++ online submissions for Longest Palindromic Substring.
-Memory Usage : 6.6 MB, less than 93.78 % of C++ online submissions for Longest Palindromic Substring.
-*/
