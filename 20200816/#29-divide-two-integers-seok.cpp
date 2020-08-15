76aab55c1562a74cb7106fbd492d473ba462f0e0
Seokjin <fietch096@gmail.com>
2020-08-15 22:47:34 +0900

    Update #29-divide-two-integers-seok.cpp

--- 20200816/#29-divide-two-integers-seok.cpp
+++ 20200816/#29-divide-two-integers-seok.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
 	int divide(int dividend, int divisor) {
-        if(dividend==INT_MIN && divisor==-1)
-        {
-            return INT_MAX;
-        }
-	int dividend_local=dividend;
-        int divisor_local = divisor;
-		int res= dividend_local/divisor_local;
-        if(res>INT_MAX || res<INT_MIN)
-        {
-            return INT_MAX;
-        }
-        int ans = res;
+		if (dividend == INT_MIN && divisor == -1)
+		{
+			return INT_MAX;
+		}
+		int dividend_local = dividend;
+		int divisor_local = divisor;
+		int res = dividend_local / divisor_local;
+		if (res > INT_MAX || res < INT_MIN)
+		{
+			return INT_MAX;
+		}
+		int ans = res;
 		return ans;
 	}
 };
