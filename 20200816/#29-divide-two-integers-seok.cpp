86051752d1185f3301261bd5dc68b6c6da1f52f8
Seokjin <fietch096@gmail.com>
2020-08-16 13:22:17 +0900

    Update #29-divide-two-integers-seok.cpp

--- 20200816/#29-divide-two-integers-seok.cpp
+++ 20200816/#29-divide-two-integers-seok.cpp
@@ -1,11 +1,14 @@
-
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+/*
+How to approach? (진행 중)
+곱하기, 나누기를 사용하지 말라는 조건이 있으므로, 쉬프트 연산자를 통해, Binary division을 실행하였다.
+현재 "while (dividendL  >= (divisorL << exp << 1))" 조건문 계산에서 overflow가 발생하여 해결책 궁리 중
+*/
 
 
 class Solution {
