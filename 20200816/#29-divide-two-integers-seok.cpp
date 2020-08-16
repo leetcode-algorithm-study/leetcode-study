46feb13d416f1ebc1d02f66b45816345d77e53e3
Seokjin <fietch096@gmail.com>
2020-08-16 10:59:44 +0900

    Update #29-divide-two-integers-seok.cpp

--- 20200816/#29-divide-two-integers-seok.cpp
+++ 20200816/#29-divide-two-integers-seok.cpp
@@ -1,18 +1,56 @@
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
 class Solution {
 public:
-	int divide(int dividend, int divisor) {
+	int divide(int dividend, int divisor)
+	{
 		if (dividend == INT_MIN && divisor == -1)
 		{
 			return INT_MAX;
 		}
-		int dividend_local = dividend;
-		int divisor_local = divisor;
-		int res = dividend_local / divisor_local;
-		if (res > INT_MAX || res < INT_MIN)
+
+		int dividendL = abs(dividend);
+		int divisorL = abs(divisor);
+		int res = 0;
+		int isNegative = false;
+
+		if (dividend < 0 && divisor>0)
 		{
-			return INT_MAX;
+			isNegative = true;
 		}
-		int ans = res;
-		return ans;
+		else if (dividend > 0 && divisor < 0)
+		{
+			isNegative = true;
+		}
+
+		while (dividendL >= divisorL)
+		{
+			int exp = 0;
+			while (dividendL  >= (divisorL << exp << 1))
+			{
+				exp++;
+			}
+			res += 1 << exp;
+			dividendL -= (divisorL << exp);
+		}
+		return isNegative ? -res : res;
 	}
 };
+
+int main()
+{
+	int dividend = 2147483647;
+	int divisor = 1;
+	Solution s;
+	s.divide(dividend, divisor);
+
+	return 0;
+}
