2cba58fd84c47d3a9765125f4938538f84906ed2
Seokjin <fietch096@gmail.com>
2020-08-17 19:47:33 +0900

    Create #33-search-in-rotated-sorted-array-seok.cpp

---
+++ 20200823/#33-search-in-rotated-sorted-array-seok.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		for (int i=0;i<nums.size();i++)
+		{
+			if (nums[i] == target)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+};
+
+int main()
+{
+	vector<int>v{ 4,5,6,7,0,1,2 };
+	int target = 0;
+	Solution s;
+	cout<<s.search(v, target);
+	return 0;
+}
+
+/*
+Runtime: 8 ms, faster than 69.90% of C++ online submissions for Search in Rotated Sorted Array.
+Memory Usage: 11.1 MB, less than 93.34% of C++ online submissions for Search in Rotated Sorted Array.
+*/
