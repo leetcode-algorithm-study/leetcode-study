2836003f9ee47637834f33b2a43b1492d2c0f103
Seokjin <fietch096@gmail.com>
2020-08-29 22:19:15 +0900

    Create group-anagrams.cpp

---
+++ 20200830/#49/group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    map<string, vector<string>> MyMap;
+	vector<vector<string>> groupAnagrams(vector<string>& input) {
+		vector<vector<string>> answer;
+		for (int i = 0; i < input.size(); i++)
+		{
+			string key = input[i];
+			sort(key.begin(), key.end());
+			MyMap[key].push_back(input[i]);
+		}
+		map < string, vector<string>>::iterator iter;
+		for (iter = MyMap.begin(); iter != MyMap.end(); iter++)
+		{
+			answer.push_back(iter->second);
+		}
+		return answer;
+	}
+};
