42c2c20477ed39714dab499d9ca13a6a491356e6
brrgi <48500985+brrgi@users.noreply.github.com>
2020-08-30 14:07:29 +0900

    Update Group-Anagrams-re.py

--- 20200830/#49/Group-Anagrams-re.py
+++ 20200830/#49/Group-Anagrams-re.py
@@ -1,6 +1,20 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        ans = collections.defaultdict(list)
-        for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return ans.values()
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        all=[]
+        for i in strs:
+            visit=[0 for i in range(26)]
+            for j in i:
+                visit[ord(j)-97]+=1
+            if visit not in all:
+                all.append(visit)
+        newOne=[[] for i in range(len(all))]
+        
+        for i in strs:
+            visit=[0 for i in range(26)]
+            for j in i:
+                visit[ord(j)-97]+=1
+            where=all.index(visit)
+
+            newOne[where].append(i)
+
+        return newOne
