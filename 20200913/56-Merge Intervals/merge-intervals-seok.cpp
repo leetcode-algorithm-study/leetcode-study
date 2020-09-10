9560e926e2bc2d25e9d0fdf0573506fd5e895037
Seokjin <fietch096@gmail.com>
2020-09-10 19:24:35 +0900

    Update merge-intervals-seok.cpp

--- 20200913/56-Merge Intervals/merge-intervals-seok.cpp
+++ 20200913/56-Merge Intervals/merge-intervals-seok.cpp
@@ -3,6 +3,7 @@
 class Solution {
 public:
 	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		vector<int> tmp;
 		if (intervals.size() <= 1)
 		{
 			return intervals;
@@ -15,7 +16,7 @@ public:
 		{
 			if (intervals[i][0] <= res.back().back())
 			{
-				vector<int> tmp = {intervals[i][0],intervals[i][1],res.back().front(),res.back().back()};
+				tmp = {intervals[i][0],intervals[i][1],res.back().front(),res.back().back()};
 				sort(tmp.begin(), tmp.end());
 				tmp = { tmp.front(),tmp.back() };
 				res.pop_back();
@@ -32,6 +33,6 @@ public:
 };
 
 /*
-Runtime: 60 ms, faster than 39.77% of C++ online submissions for Merge Intervals.
-Memory Usage: 15.6 MB, less than 21.65% of C++ online submissions for Merge Intervals.
+Runtime: 56 ms, faster than 44.51% of C++ online submissions for Merge Intervals.
+Memory Usage: 15.2 MB, less than 25.89% of C++ online submissions for Merge Intervals.
 */
