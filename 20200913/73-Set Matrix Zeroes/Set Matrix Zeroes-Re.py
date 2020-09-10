f0a2c0ad2d66b99363661332c4dd693e4e922557
brrgi <48500985+brrgi@users.noreply.github.com>
2020-09-10 15:08:48 +0900

    Update Set Matrix Zeroes-Re.py

--- 20200913/73-Set Matrix Zeroes/Set Matrix Zeroes-Re.py
+++ 20200913/73-Set Matrix Zeroes/Set Matrix Zeroes-Re.py
@@ -1,8 +1,5 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         zero=[]
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
@@ -15,4 +12,5 @@ class Solution:
             for i in range(len(matrix[0])):
                 if matrix[hiLeeRe[0]][i]!=0:
                     matrix[hiLeeRe[0]][i]=0
-                    
+# Runtime: 132 ms, faster than 92.84% of Python3 online submissions for Unique Paths.
+# Memory Usage: 14.4 MB, less than 35.56% of Python3 online submissions for Unique Paths.
