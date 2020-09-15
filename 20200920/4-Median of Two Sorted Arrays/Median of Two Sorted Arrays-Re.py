1fbd4e0a20e2cfc3de44c7b4e340fcdc6c5be611
brrgi <48500985+brrgi@users.noreply.github.com>
2020-09-15 10:26:14 +0900

    Update Median of Two Sorted Arrays-Re.py

--- 20200920/4-Median of Two Sorted Arrays/Median of Two Sorted Arrays-Re.py
+++ 20200920/4-Median of Two Sorted Arrays/Median of Two Sorted Arrays-Re.py
@@ -1,14 +1,18 @@
 import heapq
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         h = []
         length=len(nums1)+len(nums2)
+        
+        #Step 1 - init heapq
         for num in nums1:
             heapq.heappush(h, (num, num))
         for num in nums2:
             heapq.heappush(h, (num, num))
+            
+            
         result=0
+        #Step 2 - Distinguish even and odd numbers
         if length%2==0:
             for i in range(length//2-1):
                 heapq.heappop(h)
@@ -20,5 +24,6 @@ class Solution:
                 heapq.heappop(h)
             result+=heapq.heappop(h)[0]
         return result
-# Runtime: 116 ms, faster than 31.71% of Python3 online submissions for Median of Two Sorted Arrays.
-# Memory Usage: 14.2 MB, less than 23.53% of Python3 online submissions for Median of Two Sorted Arrays.
+    
+# Runtime: 112 ms, faster than 35.17% of Python3 online submissions for Median of Two Sorted Arrays.
+# Memory Usage: 14.1 MB, less than 48.27% of Python3 online submissions for Median of Two Sorted Arrays.
