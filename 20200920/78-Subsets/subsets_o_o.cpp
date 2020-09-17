aaa9d95a090410cfbaa16f129d111ff0d08927b7
o-oppang <cmal0425@gmail.com>
2020-09-18 01:48:15 +0900

    Solve #75, #78

---
+++ 20200920/78-Subsets/subsets_o_o.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subsets;
+        subsets.push_back({});
+        for(auto& target : nums ) {
+             for(int i = 0, count = subsets.size(); i < count; ++i) {
+                vector<int> subset = subsets[i];
+                subset.push_back(target);
+                subsets.push_back(subset);
+            }
+        }
+        return subsets;
+    }
+};
+
+// Runtime: 4 ms, faster than 66.32% of C++ online submissions for Subsets.
+// Memory Usage: 7.1 MB, less than 83.44% of C++ online submissions for Subsets.
