33283e897c2c7e9e221d42cca6a0af0f2146c4c5
Seokjin <fietch096@gmail.com>
2020-09-20 13:21:29 +0900

    Create sort-colors-seok.cpp

---
+++ 20200920/75-Sort Colors/sort-colors-seok.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, mid = 0, right = nums.size() - 1;
+        while (mid <= right) {
+            if (nums[mid] == 0)
+            {
+                swap(nums[left++], nums[mid++]);
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+                
+            }
+            else
+            {
+                swap(nums[mid], nums[right--]);
+            }
+        }
+    }
+};
+
+/*
+Runtime: 8 ms, faster than 9.42% of C++ online submissions for Sort Colors.
+Memory Usage: 8.7 MB, less than 5.14% of C++ online submissions for Sort Colors.
+*/
