874c052a89950d1d4a7e597c822d1139c0c0c76f
Seokjin <fietch096@gmail.com>
2020-09-20 12:17:49 +0900

    Update median-of-two-sorted-arrays-seok.cpp

--- 20200920/4-Median of Two Sorted Arrays/median-of-two-sorted-arrays-seok.cpp
+++ 20200920/4-Median of Two Sorted Arrays/median-of-two-sorted-arrays-seok.cpp
@@ -10,12 +10,44 @@ public:
         vector<int> ans;
         double median = 0.0;
         
+        
+        // Time Complexity O(m + nlogn)
+        /*
         ans = nums1;
         for (auto i : nums2)
         {
             ans.push_back(i);
         }
         sort(ans.begin(),ans.end());
+        */
+        
+        
+        //////////// Time Complexity O(m + nlogn) → O(n + m)
+        int point1 = 0;
+        int point2 = 0;
+
+        while (point1 < nums1.size() && point2 < nums2.size())
+        {
+            if (nums1[point1] <= nums2[point2])
+            {
+                ans.push_back(nums1[point1++]);
+            }
+            else
+            {
+                ans.push_back(nums2[point2++]);
+            }
+        }
+        while (point1 < nums1.size())
+        {
+            ans.push_back(nums1[point1++]);
+        }
+        while (point2 < nums2.size())
+        {
+            ans.push_back(nums2[point2++]);
+        }
+        ///////////////////////////////////////////////
+        
+        
         int n = ans.size();
         
         
@@ -32,4 +64,7 @@ public:
 /*
 Runtime: 76 ms, faster than 20.71% of C++ online submissions for Median of Two Sorted Arrays.
 Memory Usage: 89.7 MB, less than 21.67% of C++ online submissions for Median of Two Sorted Arrays.
+→
+Runtime: 60 ms, faster than 41.43% of C++ online submissions for Median of Two Sorted Arrays.
+Memory Usage: 89.9 MB, less than 11.10% of C++ online submissions for Median of Two Sorted Arrays.
 */
