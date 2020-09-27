3f539dd84f63d70d8ce501c293f5e2d804da7259
Seokjin <fietch096@gmail.com>
2020-09-27 10:55:03 +0900

    Update decode-ways-seok.cpp

--- 20200927/91. Decode Ways/decode-ways-seok.cpp
+++ 20200927/91. Decode Ways/decode-ways-seok.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
-	int cnt = 0;
-	void dfs(string str, int pos)
+	vector<int> mem;
+	int recur(string str, int pos)
 	{
 		if (pos >= str.size())
 		{
 			if (pos == str.size())
 			{
-				cnt++;
+				return 1;
 			}
-			return;
+			return 0;
 		}
-		string nextStr;
+
+		if (mem[pos] != -1)
+		{
+			return mem[pos];
+		}
+
+		int cnt = 0;
+
 		if (str[pos] != '0')
 		{
-			dfs(str,pos+1);
+
+			cnt += recur(str, pos + 1);
 
 			if (pos + 1 < str.size())
 			{
-				int maxCount = (str[pos] - '0') * 10 + (str[pos+1] - '0');
-				if (maxCount <= 26)
+				int maxCount = (str[pos] - '0') * 10 + (str[pos + 1] - '0');
+				if (maxCount <= 26 && maxCount > 9)
 				{
-					dfs(str,pos+2);
+					cnt += recur(str, pos + 2);
 				}
 			}
 		}
-
+		return mem[pos] = cnt;
 	}
 	int numDecodings(string s) {
-		dfs(s,0);
-		return cnt;
+		mem = vector<int>(s.size(), -1);
+		return recur(s, 0);
 	}
 };
+
+/*
+Runtime: 8 ms, faster than 18.96% of C++ online submissions for Decode Ways.
+Memory Usage: 14.5 MB, less than 8.09% of C++ online submissions for Decode Ways.
+*/
