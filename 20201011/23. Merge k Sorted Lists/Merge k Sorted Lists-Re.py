ad4e6aa884141f36ea6e351e7794735a9fe61c75
brrgi <48500985+brrgi@users.noreply.github.com>
2020-09-29 17:08:15 +0900

    Create Merge k Sorted Lists-Re.py

---
+++ 20201011/23. Merge k Sorted Lists/Merge k Sorted Lists-Re.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class LinkedList:
+    def __init__(self):
+        dummy = ListNode("dummy")
+        self.head = dummy
+        self.tail = dummy
+
+        self.current = None
+        self.before = None
+
+        self.num_of_data = 0
+        
+    # append 메소드 (insert - 맨 뒤에 노드 추가, tail과 node의 next, 데이터 개수 변경)
+    def append(self, data):
+        new_node = ListNode(data)
+        self.tail.next = new_node
+        self.tail = new_node
+
+        self.num_of_data += 1
+        
+        
+        
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dic=dict()
+        leeRe=LinkedList()
+        if lists==[]:
+            return leeRe.head.next
+        
+        for list in lists:
+            if list==[] or list==None:
+                continue
+            while 1:
+                if list.val not in dic.keys():
+                    dic[list.val]=1
+                else:
+                    dic[list.val]+=1
+                if list.next!=None:
+                    list=list.next
+                else:
+                    break
+        if len(dic)==0:
+            return leeRe.head.next
+        
+        t=sorted(dic.items())
+        leeRe=LinkedList()
+        for node in t:
+            for _ in range(node[1]):
+                leeRe.append(node[0])
+        return leeRe.head.next
+    
+# Runtime: 104 ms, faster than 82.46% of Python3 online submissions for Merge k Sorted Lists.
+# Memory Usage: 18.3 MB, less than 15.68% of Python3 online submissions for Merge k Sorted Lists.
