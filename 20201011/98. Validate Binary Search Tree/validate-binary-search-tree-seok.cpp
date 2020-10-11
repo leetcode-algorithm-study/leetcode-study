7b3557cb72d39846e3e61367154f19f4486443a6
Seokjin <fietch096@gmail.com>
2020-10-11 13:30:53 +0900

    Create validate-binary-search-tree-seok.cpp

---
+++ 20201011/98. Validate Binary Search Tree/validate-binary-search-tree-seok.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+	bool validRecursive(TreeNode* root, long min, long max)
+	{
+		bool chk = true;
+		if (root != nullptr)
+		{
+			chk &= (root->val > min);
+			chk &= validRecursive(root->left, min, root->val);
+			chk &= (root->val < max);
+			chk &= validRecursive(root->right, root->val, max);
+		}
+		return chk;
+	}
+	bool validIterative(TreeNode* root) {
+		long threshold = LONG_MIN;
+		stack<TreeNode*> st;
+		while (root || !st.empty()) {
+			while (root) {
+				st.push(root);
+				root = root->left;
+			}
+			root = st.top();
+			st.pop();
+			if (root->val <= threshold) {
+				return false;
+			}
+			threshold = root->val;
+			root = root->right;
+		}
+		return true;
+	}
+	bool isValidBST(TreeNode* root) {
+		return validRecursive(root, LONG_MIN, LONG_MAX);
+		//return validIterative(root);
+	}
+};
+
+/*
+Recursive & Iterative (Same result)
+Runtime: 16 ms
+Memory Usage: 22 MB
+*/
