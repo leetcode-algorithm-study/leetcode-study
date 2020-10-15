a214c16b690e76cae5088dd0ca63d0733cd90940
brrgi <48500985+brrgi@users.noreply.github.com>
2020-10-15 10:24:31 +0900

    Create First Missing Positive-Re.py

---
+++ 20201018/41. First Missing Positive/First Missing Positive-Re.py
@@ -0,0 +1,18 @@
+import sys
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        dic=dict()
+        for num in nums:
+            if num>0:
+                dic[num]=1
+        first=1
+        while 1:
+            if first not in dic:
+                return first
+            else:
+                first+=1
+        print(first)
+        return first
+
+# Runtime: 32 ms, faster than 83.63% of Python3 online submissions for First Missing Positive.
+# Memory Usage: 14.1 MB, less than 100.00% of Python3 online submissions for First Missing Positive.
