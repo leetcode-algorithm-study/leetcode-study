65a2905aa9d8b95f2cf05f44a0c37970c1981757
김혜리 <hyer1k@naver.com>
2020-10-17 19:47:33 +0900

    solve number 103

---
+++ 20201018/103.Binary Tree Zigzag Level Order Traversal/BinaryTreeZigzagLevelOrderTraversal_kimhyeri.swift
@@ -0,0 +1,98 @@
+//
+//  BinaryTreeZigzagLevelOrderTraversal.swift
+//  LeetCode
+//
+//  Created by 김혜리 on 2020/10/17.
+//  Copyright © 2020 hyerikim. All rights reserved.
+//
+
+import Foundation
+
+/// 103. Binary Tree Zigzag Level Order Traversal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+    guard let rootnode = root else { return [] }
+    var result: [[Int]] = []
+    var zigzag: Bool = true
+    
+    // 순회하기 위한 배열
+    var array: [TreeNode?] = [TreeNode]()
+    array.append(rootnode)
+    
+    while(!array.isEmpty) {
+        var count = array.count - 1
+        var level = [Int]()
+        
+        // 차례로 왼쪽 오른쪽 가기 위해서
+        while(count >= 0) {
+            if let node = array.removeFirst() {
+                level.append(node.val)
+                
+                if let left = node.left {
+                    array.append(left)
+                }
+                if let right = node.right {
+                    array.append(right)
+                }
+                count = count - 1
+            }
+        }
+        if zigzag {
+            level.reverse()
+        }
+        zigzag.toggle()
+        result.append(level)
+    }
+    
+    return result
+}
+
+let tree = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+print(zigzagLevelOrder(tree))
+
+//        3
+//       / \
+//      9  20
+//        /  \
+//       15   7
