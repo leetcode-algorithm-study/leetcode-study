e02bae1fb1e5c267406788da202a562b2345cdfe
Seokjin <fietch096@gmail.com>
2020-11-08 16:40:46 +0900

    problem#44 memoization added & comment added.

--- 20201108/127. Word Ladder/word-ladder-seok.cpp
+++ 20201108/127. Word Ladder/word-ladder-seok.cpp
@@ -1,5 +1,10 @@
-// DFS, but TLE
-
+/*
+DFS, but TLE
+Last executed input:
+"qa"
+"sq"
+["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]
+*/
 class Solution {
 private:
 	int minPath = INT_MAX;
--- 20201108/44. Wildcard Matching/wildcard-matching-seok.cpp
+++ 20201108/44. Wildcard Matching/wildcard-matching-seok.cpp
@@ -1,33 +1,64 @@
+/* memoization, but TLE
+Last executed input:
+"aaabbaabbbbbaabbbabbbaaabbbaaaabaaabaaaabbbbaabbbabaababababbaabbaabbbbaabaabbaabbaaaabbbbaaaabbaaabbaabbaaabababbabaaabbaabababbbaabaaaaaabababbaababaababaaabbbbaaaaaaaaaaaabbababaababaabababaabaabbaaba"
+"*bb*bb*****b******a**ab*bba****ba*a*a*aa*abb*baa*a**ba**b*ba**b**ba*ab**bb***a*ba*ab****ab**aab*bb*b"
+*/
 class Solution {
 public:
-	bool isMatchWord(string s, string p, int i, int j)
+	bool isMatchWord(vector<vector<int>>& dp, string s, string p, int i, int j)
 	{
-		if (s[i] == '\0' && p[j] == '\0')
+		if (i == s.size() && j == p.size())
 		{
 			return true;
 		}
+
+		if (i == s.size())
+		{
+			if (p[j] == '*')
+			{
+				return isMatchWord(dp, s, p, i, j + 1);
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		if (j == p.size())
+		{
+			return false;
+		}
+
+		if (dp[i][j] != -1)
+		{
+			return dp[i][j];
+		}
+
 		if (s[i] == p[j] || p[j] == '?')
 		{
-			return isMatchWord(s, p, i + 1, j + 1);
+			return dp[i][j] = isMatchWord(dp, s, p, i + 1, j + 1);
 		}
 		if (p[j] == '*')
 		{
 			if (j == p.size() - 1)
 			{
-				return true;
+				return dp[i][j] = true;
 			}
 			else if (i == s.size() - 1)
 			{
-				return isMatchWord(s, p, i, j + 1);
+				return dp[i][j] = isMatchWord(dp, s, p, i, j + 1);
 			}
 			else
 			{
-				return isMatchWord(s, p, i + 1, j) || isMatchWord(s, p, i, j + 1);
+				return dp[i][j] = (isMatchWord(dp, s, p, i + 1, j) || isMatchWord(dp, s, p, i, j + 1));
 			}
 		}
-		return false;
+		return dp[i][j] = false;
 	}
 	bool isMatch(string s, string p) {
-		return isMatchWord(s, p, 0, 0);
+
+		vector<vector<int>> dp(s.size(), vector<int>(p.size(), -1));
+
+		return isMatchWord(dp, s, p, 0, 0);
 	}
 };
