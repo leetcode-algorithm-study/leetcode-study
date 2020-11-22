e1334f59df7f13ae9402eaaf4ec8a066671fa7c7
brrgi <48500985+brrgi@users.noreply.github.com>
2020-11-22 16:35:44 +0900

    Update PalindromePartitioning_Re.py

--- 20201115/131. Palindrome Partitioning/PalindromePartitioning_Re.py
+++ 20201115/131. Palindrome Partitioning/PalindromePartitioning_Re.py
@@ -18,5 +18,25 @@ class Solution(object):
         dfs(s,[],res)
         return res
         
-        
+#Solution 2 - DP (Not Solved)
       
+# class Solution:
+    
+#     def partition(self, s: str) -> List[List[str]]:
+#         def IsPalindrome(a):
+#             if a==a[::-1]:
+#                 return True
+#             return False
+#         if s=='':
+#             return []
+        
+#         dp=[[0 for i in range(len(s))] for i in range(len(s))]
+#         for i in range(len(s)):
+#             for j in range(i+1,len(s)+1):
+#                 if IsPalindrome(s[i:j])==True:
+#                     dp[i][j-1]=1
+        
+#         for i in dp:
+#             print(i)
+            
+        
