7ce53fe0919fcf9bd3d62cb1fc49527a17276f7a
Seokjin <fietch096@gmail.com>
2020-12-05 12:50:26 +0900

    146 solved

---
+++ 20201206/146. lru-cache/lru-cahce-seok.cpp
@@ -0,0 +1,50 @@
+class LRUCache {
+public:
+	unordered_map<int, int> cap;
+	unordered_map<int, int>capUsedCnt;
+	int idxCnt = 0;
+	int capSize = 0;
+	int dateCapUsed = 0;
+	LRUCache(int capacity) {
+		capSize = capacity;
+	}
+
+	int get(int key) {
+		if (cap.find(key) == cap.end())
+		{
+			return -1;
+		}
+		else
+		{
+
+			capUsedCnt[key] = ++dateCapUsed;
+			return cap[key];
+		}
+	}
+	void put(int key, int value) {
+		if (cap.find(key) == cap.end())
+		{
+			if (idxCnt < capSize)
+			{
+				++idxCnt;
+				cap.insert(pair<int, int>(key, value));
+				capUsedCnt.insert(pair<int, int>(key, 0));
+			}
+			else
+			{
+				auto minUsedCnt = *min_element(capUsedCnt.begin(), capUsedCnt.end(), [](const auto& lhs, const auto& rhs) {
+					return lhs.second < rhs.second; });
+				int minUsedKey = minUsedCnt.first;
+				cap.erase(minUsedKey);
+				capUsedCnt.erase(minUsedKey);
+				cap.insert(pair<int, int>(key, value));
+				capUsedCnt.insert(pair<int, int>(key, value));
+			}
+		}
+		else
+		{
+			cap[key] = value;
+		}
+		capUsedCnt[key] = ++dateCapUsed;
+	}
+};
