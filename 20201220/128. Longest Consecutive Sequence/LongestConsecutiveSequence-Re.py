7733de76d4aaf37a791e504ce5ad0e88d78f88d2
brrgi <48500985+brrgi@users.noreply.github.com>
2021-01-02 22:37:48 +0900

    2021/01/02

--- 20201220/128. Longest Consecutive Sequence/LongestConsecutiveSequence-Re.py
+++ 20201220/128. Longest Consecutive Sequence/LongestConsecutiveSequence-Re.py
@@ -1,18 +1,42 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        #First Fail Solution
+        # result = 0
+        # numChecker = [0 for _ in range(2 * (10 ** 9) + 1)]
+        # for num in nums:
+        #     numChecker[num + 10 ** 9] = 1
+        #
+        # now = 0
+        # for checker in numChecker:
+        #     if checker == 1:
+        #         now += 1
+        #     else:
+        #         if result < now:
+        #             result = now
+        #         now = 0
+        #
+        # print(result)
+
+
+        #Second Success Solution
+        if nums == []:
+            return 0
+
         result = 0
-        numChecker = [0 for _ in range(2 * (10 ** 9) + 1)]
-        for num in nums:
-            numChecker[num + 10 ** 9] = 1
+        minValue = min(nums)
+        maxValue = max(nums)
+        newNums = set(nums)
 
         now = 0
-        for checker in numChecker:
-            if checker == 1:
+
+        for i in range(minValue, maxValue + 2):
+            if i in newNums:
                 now += 1
             else:
                 if result < now:
                     result = now
                 now = 0
 
-        print(result)
-# Memory Limit Exceeded
+        return result
+    # Runtime: 76 ms, faster than 18.77% of Python3 online submissions for Longest Consecutive Sequence.
+    # Memory Usage: 15.4 MB, less than 42.95% of Python3 online submissions for Longest Consecutive Sequence.
