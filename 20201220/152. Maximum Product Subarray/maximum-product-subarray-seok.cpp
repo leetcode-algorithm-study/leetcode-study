1862819c08aac087755e41df6eab0a201e2dee20
Seokjin <fietch096@gmail.com>
2021-01-02 21:10:28 +0900

    solved #152, but solution referred

--- 20201220/152. Maximum Product Subarray/maximum-product-subarray-seok.cpp
+++ 20201220/152. Maximum Product Subarray/maximum-product-subarray-seok.cpp
@@ -41,3 +41,46 @@ public:
 		return maxVal;
 	}
 };
+
+//Runtime: 0 ms
+//Memory Usage: 12.1 MB
+class Solution {
+private:
+	void findMax(vector<int>& nums, int initIdx, int subLength, int& maxVal)
+	{
+		int product = 1;
+		int length = subLength + initIdx;
+		int productDivided;
+		int afterZero = 0;
+
+		for (int i = initIdx; i < length; i++)
+		{
+			if (nums[i] == 0)
+			{
+               			product = 1;
+				continue;
+			}
+			product *= nums[i];
+			maxVal = max(maxVal, product);
+		}
+		product = 1;
+		for (int i = length - 1; i >= 0; i--)
+		{
+			product *= nums[i];
+			maxVal = max(maxVal, product);
+			if (nums[i] == 0)
+			{
+				product = 1;
+			}
+		}
+	}
+public:
+	int maxProduct(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+		int size = nums.size();
+		int maxVal = 0;
+		int product = 1;
+		findMax(nums, 0, size, maxVal);
+		return maxVal;
+	}
+};
