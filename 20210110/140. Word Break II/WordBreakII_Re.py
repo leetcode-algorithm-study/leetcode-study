88fc983946c8e7d716c1f61e638c0482333dd5e4
brrgi <48500985+brrgi@users.noreply.github.com>
2021-01-17 17:13:19 +0900

    2021/01/10

--- 20210110/140. Word Break II/WordBreakII_Re.py
+++ 20210110/140. Word Break II/WordBreakII_Re.py
@@ -5,9 +5,10 @@ def wordBreak(s, wordDict):
     def dfs(i):
         if i == len(s):     #i가 범위를 넘으면
             return [""]
-        if i in dic:        #dic안에 i가 있으면면
+        if i in dic:        #dic안에 i가 있으면,
 
             return dic[i]
+
         result = []
         for j in range(i, len(s)):
             prefix = s[i:j + 1]
@@ -16,17 +17,15 @@ def wordBreak(s, wordDict):
                 tmp = dfs(j + 1)
                 for string in tmp:
                     string = prefix + " " + string
-                    result.append(string.strip())
+                    result.append(string.strip())       #strip는 오른쪽 공백을 지움 , 제일 뒤에 붙는 값은 prefix+" "이므로
 
         dic[i] = result
-        print("TL", dic)
-        print(result)
-        print()
         return result
 
     dic = {}
-
     wordSet = set(wordDict)
+    #wordDict = ["cat", "cats", "and", "sand", "dog"]
+    #       ->{"cat", "cats", "and", "sand", "dog"}     ->  검색 시간 단축을 위한 작업
     return dfs(0)
 
 wordBreak(s, wordDict)
--- 20210110/166. Fraction to Recurring Decimal/FractiontoRecurringDecimal_Re.py
+++ 20210110/166. Fraction to Recurring Decimal/FractiontoRecurringDecimal_Re.py
@@ -1,11 +1,32 @@
-# numerator=1
-# denominator=2
-# d = 1
-# x = (numerator / denominator)
-#
-# while 1:
-#     print((10 ** d) * x, int((10 ** d) * x) + x)
-#     if (10 ** d) * x == int((10 ** d) * x) + x:
-#         print(d)
-#         break
-#     d += 1
+def shorthead(sentence):
+    x = len(sentence)
+    for i in range(1, (x // 2)+1):
+        if(x % i == 0): #문자의 총 길이를 나눌 수 있는 수,  즉 2배 3배 4배 정확하게 떨어지면
+            s = sentence[0:i]
+            count = int(x / i)
+            if (s*count == sentence):
+                return '('+s+')'
+    return sentence
+
+numerator=1
+denominator=6
+i=0
+first=''
+string=''
+while i<10002:
+    #몫과 나머지
+    quotient=numerator//denominator
+    remainder=numerator%denominator
+    if remainder==0:
+        string+=str(quotient)
+        break
+    else:
+        if string=='':
+            first+=str(quotient)+'.'
+        numerator*=10
+        string+=str(numerator//denominator)
+        numerator=numerator%denominator
+    if numerator==0:
+        break
+    i+=1
+print(first+shorthead(string))
