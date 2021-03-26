3394ac92b999f5ee9567d278676c89708184c5c5
김혜리 <hyer1k@naver.com>
2021-03-27 02:37:23 +0900

    update 295

--- 20210328/295. Find Median from Data Stream/FindMedianfromDataStream_kimhyeri.swift
+++ 20210328/295. Find Median from Data Stream/FindMedianfromDataStream_kimhyeri.swift
@@ -11,26 +11,48 @@ import Foundation
 /// 295. Find Median from Data Stream
 
 class MedianFinder {
-
     /** initialize your data structure here. */
     var array: [Int]
     
     init() {
-        // doing something
         array = [Int]()
     }
     
     func addNum(_ num: Int) {
-        array.append(num)
-        array.sort()
+        if array.count == 0 {
+            array.append(num)
+        } else {
+            var lo = 0
+            var hi = array.count
+            while lo < hi {
+                let mid = (lo + hi)/2
+                
+                if num >= array[mid] && (mid+1 >= array.count || num <= array[mid+1]) {
+                    array.insert(num, at: mid+1)
+                    break
+                }
+                else if num <= array[mid] && (mid-1 < 0 || num >= array[mid-1]) {
+                    array.insert(num, at: mid)
+                    break
+                }
+                else if num > array[mid] {
+                    lo = mid
+                }
+                else if num < array[mid] {
+                    hi = mid
+                }
+            }
+        }
     }
     
     func findMedian() -> Double {
+        // 짝수일 때
         if array.count % 2 == 0 {
             var half = array.count / 2
             var sum = array[half - 1] + array[half]
             return Double(sum) / 2.0
         }
+        // 홀수일 때 사용
         else {
             var half = array.count / 2
             return Double(array[half])
