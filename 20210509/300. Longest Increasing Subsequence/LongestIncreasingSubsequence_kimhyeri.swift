2cf7e54057005fb4d43916fb12e0cad611fedbeb
Ellie Kim <hyer1k@naver.com>
2021-05-16 16:22:34 +0900

    Update LongestIncreasingSubsequence_kimhyeri.swift

--- 20210509/300. Longest Increasing Subsequence/LongestIncreasingSubsequence_kimhyeri.swift
+++ 20210509/300. Longest Increasing Subsequence/LongestIncreasingSubsequence_kimhyeri.swift
@@ -2,28 +2,32 @@ class Solution {
     func lengthOfLIS(_ nums: [Int]) -> Int {
         guard !nums.isEmpty else { return 0 }
 
-        var tails = [nums[0]]
+        var array = [nums.first!]
         var length = 0
         
         for num in nums {
             var left = 0
-            var right = tails.count - 1
+            var right = array.count - 1
             var middle = 0
+            
             while left != right {
                 middle = (left + right) / 2
-                if tails[middle] < num {
+                if array[middle] < num {
                     left = middle + 1
                 } else {
                     right = middle
                 }
             }
             
-            if left == tails.count - 1 && tails[left] < num {
-                tails.append(num)
+            if left == array.count - 1 && array[left] < num {
+                array.append(num)
             } else {
-                tails[left] = num
+                array[left] = num
             }
         }
-        return tails.count
+        
+        print(array)
+        
+        return array.count
     }
 }
