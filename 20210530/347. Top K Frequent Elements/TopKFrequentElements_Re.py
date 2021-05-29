e2e7301d3b4e0f482aed2e8224465b84b1385683
이레 <sksmsdlfpek@naver.com>
2021-05-30 02:51:42 +0900

    20210530

---
+++ 20210530/347. Top K Frequent Elements/TopKFrequentElements_Re.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        res = []
+        dic = dict()
+
+        for i in range(-100001, 100001):
+            dic[i] = 0
+
+        for num in nums:
+            dic[num] += 1
+
+        sdict = sorted(dic.items(), reverse=True, key=lambda item: item[1])
+        # print(sdict)
+        for i in range(k):
+            res.append(sdict[i][0])
+
+        res.sort()
+        return res
+
+# Runtime: 1760 ms, faster than 5.04% of Python online submissions for Top K Frequent Elements.
+# Memory Usage: 65.9 MB, less than 5.27% of Python online submissions for Top K Frequent Elements.
