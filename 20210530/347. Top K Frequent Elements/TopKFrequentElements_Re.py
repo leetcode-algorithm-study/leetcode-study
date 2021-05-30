5683e239799d4cbbd7dfaa4426002ac17fd7c3c2
이레 <sksmsdlfpek@naver.com>
2021-05-30 17:36:51 +0900

    20210530

--- 20210530/347. Top K Frequent Elements/TopKFrequentElements_Re.py
+++ 20210530/347. Top K Frequent Elements/TopKFrequentElements_Re.py
@@ -10,11 +10,11 @@ class Solution(object):
             dic[num] += 1
 
         sdict = sorted(dic.items(), reverse=True, key=lambda item: item[1])
-        # print(sdict)
+
         for i in range(k):
             res.append(sdict[i][0])
 
-        res.sort()
+
         return res
 
 # Runtime: 1760 ms, faster than 5.04% of Python online submissions for Top K Frequent Elements.
--- 20210530/371. Sum of Two Integers/SumOfTwoIntegers_Re.py
+++ 20210530/371. Sum of Two Integers/SumOfTwoIntegers_Re.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def getSum(self, a, b):
+
         firstValue = 0xffffffff
 
         while b:
@@ -8,9 +9,18 @@ class Solution(object):
             a = sums
             b = carry
 
-        if (a >> 31) & 1:
+        if (a >> 31) & 1:       #음수인 경우
             return ~(a ^ firstValue)
         return a
 
 # Runtime: 16 ms, faster than 69.90% of Python online submissions for Sum of Two Integers.
 # Memory Usage: 13.5 MB, less than 12.24% of Python online submissions for Sum of Two Integers.
+
+
+
+# 2   3
+# 10  11                                                  111(7)   010(2)  \  101(5)   100(4) \  1001(9)
+#
+# ->  01      xor
+# -> 100      and carry 1 왼쪽
+# ------->    101(5)
