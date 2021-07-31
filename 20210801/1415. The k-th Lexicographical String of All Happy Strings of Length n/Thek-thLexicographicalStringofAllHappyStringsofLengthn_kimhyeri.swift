aab395cea3b84f9211cd657334a76675f588f90d
kimhyeri <hyer1k@naver.com>
2021-07-31 19:54:55 +0900

    add #1415

---
+++ 20210801/1415. The k-th Lexicographical String of All Happy Strings of Length n/Thek-thLexicographicalStringofAllHappyStringsofLengthn_kimhyeri.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  Leetcode
+//
+//  Created by ELLIE on 2021/06/27.
+//
+
+import Foundation
+
+class Solution {
+    func getHappyString(_ n: Int, _ k: Int) -> String {
+        var temp = ""
+        var letters = ["a", "b", "c"]
+        var answer = [String]()
+        
+        func dfs(_ temp: String, _ n: Int) {
+            if temp.count == n {
+                answer.append(temp)
+                return
+            } else {
+                for i in 0..<letters.count {
+                    if temp.count == 0 {
+                        var cur = temp + letters[i]
+                        dfs(cur, n)
+                    } else {
+                        var char = letters[i]
+                        if char != String(temp.last!) {
+                            var cur = temp + char
+                            dfs(cur, n)
+                        }
+                    }
+                }
+            }
+        }
+        dfs(temp, n)
+        
+        if answer.count < k {
+            return ""
+        } else {
+            return answer[k-1]
+        }
+    }
+}
