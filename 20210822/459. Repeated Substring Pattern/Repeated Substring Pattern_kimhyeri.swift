d1e1d68f4e74ad14f98ce6e16ad02b6dd76ce9bd
kimhyeri <hyer1k@naver.com>
2021-08-22 04:47:10 +0900

    Update Repeated Substring Pattern_kimhyeri.swift

--- 20210822/459. Repeated Substring Pattern/Repeated Substring Pattern_kimhyeri.swift
+++ 20210822/459. Repeated Substring Pattern/Repeated Substring Pattern_kimhyeri.swift
@@ -9,7 +9,27 @@ import Foundation
 
 class Solution {
     func repeatedSubstringPattern(_ s: String) -> Bool {
-        for i in stride(from: s.count/2, to: 0, by: -1) {
+        var start = 0
+        var arr = Array(s)
+        
+        // TLE벗어나려는 발악/.../
+        if arr.count == 1 { 
+            return false 
+        }
+        
+        if (Set(arr).count == 1) { 
+            return true 
+        }
+        
+        for i in 1...s.count / 2 {
+            if arr[0] == arr[i] {
+                start = i
+            } else {
+                break
+            }
+        }
+        
+        for i in stride(from: s.count/2, to: start, by: -1) {
             let prefixIndex = s.index(s.startIndex, offsetBy: i)
             let suffixIndex = s.index(s.endIndex, offsetBy: -i)
 
@@ -24,7 +44,7 @@ class Solution {
                 } else {
                     if middle.replacingOccurrences(of: prefix, with: "").isEmpty {
                         return true
-                    }
+                    } 
                 }
             }
         }
