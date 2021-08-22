4e72285a67f8bee51e96be4640f356e6d4453c5e
choi1204 <76509935+choi1204@users.noreply.github.com>
2021-08-22 16:06:31 +0900

    feat:leetcode_686

---
+++ 20210822/686. Repeated String Match/Repeated String Match_choihyunwoong.java
@@ -0,0 +1,20 @@
+
+public class Solution_686 {
+
+    public static int repeatedStringMatch(String a, String b) {
+        StringBuilder str=new StringBuilder();
+        int count=0;
+
+        while(str.toString().length() < a.length()+b.length() ){
+            if(str.toString().contains(b))
+                return count;
+            str.append(a);
+            count++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        repeatedStringMatch("abcd","cdabcdab");
+    }
+}
