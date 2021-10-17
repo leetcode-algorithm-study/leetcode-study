8fc8c855bc0f5a9066555de7ab78472fece52878
kimhyeri <hyer1k@naver.com>
2021-10-17 16:16:54 +0900

    resolved 1023

--- 20211010/1023. Camelcase Matching/Camelcase Matching_kimhyeri.swift
+++ 20211010/1023. Camelcase Matching/Camelcase Matching_kimhyeri.swift
@@ -9,6 +9,25 @@ import Foundation
 
 class Solution {
     func camelMatch(_ queries: [String], _ pattern: String) -> [Bool] {
-        
+        var answer =  [Bool]()
+        let patternArray = Array(pattern)
+
+        for query in queries {
+            var index = 0
+            var isMatching = true
+
+            for char in query {
+                if index < patternArray.count && char == patternArray[index] {
+                    index = index + 1
+                } else if char.isUppercase {
+                    isMatching = false
+                    break
+                }
+            }
+            isMatching = isMatching && index == patternArray.count
+            answer.append(isMatching)
+        }
+
+        return answer
     }
 }
