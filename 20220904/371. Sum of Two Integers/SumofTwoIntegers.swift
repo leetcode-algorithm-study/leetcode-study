1d63a847d5838caab4cf447efaad4d3974eff27d
kimhyeri <hyer1k@naver.com>
2022-09-15 12:47:09 +0900

    update answer

--- 20220904/371. Sum of Two Integers/SumofTwoIntegers.swift
+++ 20220904/371. Sum of Two Integers/SumofTwoIntegers.swift
@@ -1,5 +1,20 @@
 class Solution {
     func getSum(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let carry = (a & b) << 1
+            a = a ^ b
+            b = carry
+        }
+        
+        return a
+    }
+}
+
+class Solution {
+    func getSum(_ a: Int, _ b: Int) -> Int {
         return a.advanced(by: b)
     }
 }
