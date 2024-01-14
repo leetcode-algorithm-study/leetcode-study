37ff0f977e9e16410dda803a1ac3983434cc6b49
kimhyeri <hyer1k@naver.com>
2024-01-14 11:08:37 +0900

    solve #100, #124, #226

---
+++ 20231119/226. Invert Binary Tree/InvertBinaryTree_kimhyeri.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else { return nil }
+        let leftTree = root.left
+        let rightTree = root.right
+        
+        root.left = rightTree
+        root.right = leftTree
+        
+        invertTree(root.left)
+        invertTree(root.right)
+
+        return root
+    }
+}
