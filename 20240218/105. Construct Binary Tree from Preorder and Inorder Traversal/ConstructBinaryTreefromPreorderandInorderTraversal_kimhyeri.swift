1fe354fbef89d3da2fae948ca12335b9b8716784
kimhyeri <hyer1k@naver.com>
2024-02-25 14:28:36 +0900

    Update #105

--- 20240218/105. Construct Binary Tree from Preorder and Inorder Traversal/ConstructBinaryTreefromPreorderandInorderTraversal_kimhyeri.swift
+++ 20240218/105. Construct Binary Tree from Preorder and Inorder Traversal/ConstructBinaryTreefromPreorderandInorderTraversal_kimhyeri.swift
@@ -15,20 +15,17 @@
  */
 class Solution {
     func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+        guard !inorder.isEmpty && !preorder.isEmpty else { return nil }
         
-        func builder(inorder: [Int], preorder: [Int]) -> TreeNode? {
-            guard !inorder.isEmpty && !preorder.isEmpty else { return nil }
-            let rootVal = preorder[0]
-            var rootNode = TreeNode(rootVal)
-            let mid = inorder.index(of: rootVal)!
+        let rootVal = preorder[0]
+        var rootNode = TreeNode(rootVal)
+        let mid = inorder.index(of: rootVal)!
 
-            rootNode.left = builder(inorder: Array(inorder[...mid]), preorder: Array(preorder[1..<(mid + 1)]))
-            rootNode.right = builder(inorder: Array(inorder[(mid+1)...]), preorder: Array(preorder[(mid + 1)...]))
+        print("preorder:\(preorder) mid:\(mid) inorder\(inorder)")
+        rootNode.left = buildTree(Array(preorder[1..<(mid + 1)]), Array(inorder[...mid]))
+        rootNode.right = buildTree(Array(preorder[(mid + 1)...]), Array(inorder[(mid+1)...]))
 
-            return rootNode
-        }
-        
-        return builder(inorder: inorder, preorder: preorder)
+        return rootNode
     }
 
 }
